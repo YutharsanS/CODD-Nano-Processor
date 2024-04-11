f9119c62f1a9754a58796b36e8927d9ddd776ebf
YutharsanS <yutharsan.22@cse.mrt.ac.lk>
2024-04-11 16:29:33 +0530

    Added project folder(not implemented) for ROM

---
+++ Program ROM/Program ROM.srcs/sources_1/new/Program_ROM_8.vhd
@@ -0,0 +1,13 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity Program_ROM_8 is
+  --Port ( );
+end Program_ROM_8;
+
+architecture Behavioral of Program_ROM_8 is
+
+begin
+
+
+end Behavioral;
