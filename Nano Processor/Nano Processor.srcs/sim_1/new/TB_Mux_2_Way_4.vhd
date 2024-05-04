59f0c43ad2f8888a7665751874e65878b65d6f83
Vihanga Munasinghe <149761186+VihangaMunasinghe@users.noreply.github.com>
2024-05-04 15:52:59 +0530

    Indexes included into Mux_2_way_4 sim

--- Nano Processor/Nano Processor.srcs/sim_1/new/TB_Mux_2_Way_4.vhd
+++ Nano Processor/Nano Processor.srcs/sim_1/new/TB_Mux_2_Way_4.vhd
@@ -5,63 +5,65 @@ entity Mux_2_way_4_tb is
 end Mux_2_way_4_tb;
 
 architecture Behavioral of Mux_2_way_4_tb is
-    component Mux_2_way_4
-        port (
-            A      : in std_logic_vector(3 downto 0);
-            B      : in std_logic_vector(3 downto 0);
-            Sel    : in std_logic;
-            Output : out std_logic_vector(3 downto 0)
-        );
-    end component;
+  component Mux_2_way_4
+    port
+    (
+      A      : in std_logic_vector(3 downto 0);
+      B      : in std_logic_vector(3 downto 0);
+      Sel    : in std_logic;
+      Output : out std_logic_vector(3 downto 0)
+    );
+  end component;
 
-    signal A_tb      : std_logic_vector(3 downto 0);
-    signal B_tb      : std_logic_vector(3 downto 0);
-    signal Sel_tb    : std_logic;
-    signal Output_tb : std_logic_vector(3 downto 0);
+  signal A_tb      : std_logic_vector(3 downto 0);
+  signal B_tb      : std_logic_vector(3 downto 0);
+  signal Sel_tb    : std_logic;
+  signal Output_tb : std_logic_vector(3 downto 0);
 
 begin
-    UUT : Mux_2_way_4
-    port map (
-        A      => A_tb,
-        B      => B_tb,
-        Sel    => Sel_tb,
-        Output => Output_tb
-    );
+  UUT : Mux_2_way_4
+  port map
+  (
+    A      => A_tb,
+    B      => B_tb,
+    Sel    => Sel_tb,
+    Output => Output_tb
+  );
 
-    stimulus_process : process
-    begin
+  stimulus_process : process
+  begin
 
-        A_tb <= "0101";
-        B_tb <= "1010";
-        Sel_tb <= '0';
-        wait for 100 ns;
-        assert Output_tb = "0101" report "Selection failed for 0"  severity error;
+    --Index1 : 220038U - 10 1101 1011 1000 0110
+    --Index2 : 220404N - 10 1101 1100 1111 0100
+    --Index3 : 220681H - 10 1101 1110 0000 1001
+    --Index4 : 220738P - 10 1101 1110 0100 0010
 
+    A_tb   <= "0110";
+    B_tb   <= "1001";
+    Sel_tb <= '0';
+    wait for 100 ns;
+    assert Output_tb = "0110" report "Selection failed for 0" severity error;
 
-        Sel_tb <= '1';
-        wait for 100 ns;
-        assert Output_tb = "1010" report "Selection failed for 1"  severity error;
+    Sel_tb <= '1';
+    wait for 100 ns;
+    assert Output_tb = "1001" report "Selection failed for 1" severity error;
 
+    A_tb   <= "1100";
+    B_tb   <= "1100";
+    Sel_tb <= '0';
+    wait for 100 ns;
+    assert Output_tb = "1100" report "Selection failed for same number" severity error;
 
-       A_tb <= "1111";
-       B_tb <= "1111";
-       Sel_tb <= '0';
-       wait for 100 ns;
-       assert Output_tb = "1111" report "Selection failed for same number" severity error;
+    Sel_tb <= '1';
+    wait for 100 ns;
+    assert Output_tb = "1111" report "Selection failed for same number" severity error;
 
-       A_tb <= "1111";
-       B_tb <= "1111";
-       Sel_tb <= '1';
-       wait for 100 ns;
-       assert Output_tb = "1111" report "Selection failed for same number" severity error;
+    A_tb   <= "1101";
+    B_tb   <= "0010";
+    Sel_tb <= '0';
+    wait for 100 ns;
+    assert Output_tb = "1101" report "Selection failed for random test" severity error;
 
-
-        A_tb <= "1011";
-        B_tb <= "0000";
-        Sel_tb <= '0';
-        wait for 100 ns;
-        assert Output_tb = "1011" report "Selection failed for random test" severity error;
-
-        wait;
-    end process;
+    wait;
+  end process;
 end Behavioral;
