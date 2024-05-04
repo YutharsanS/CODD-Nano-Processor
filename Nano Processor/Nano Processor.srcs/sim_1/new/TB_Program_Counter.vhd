c941afb3b0a3f91adb914277027790af93645435
Vihanga Munasinghe <149761186+VihangaMunasinghe@users.noreply.github.com>
2024-05-04 15:57:49 +0530

    Inserted indexes into Program_Counter sim

--- Nano Processor/Nano Processor.srcs/sim_1/new/TB_Program_Counter.vhd
+++ Nano Processor/Nano Processor.srcs/sim_1/new/TB_Program_Counter.vhd
@@ -41,22 +41,41 @@ begin
   -- Test cases
   process
   begin
+
+    --Index1 : 220038U - 101 101 101 110 000 110
+    --Index2 : 220404N - 101 101 110 011 110 100
+    --Index3 : 220681H - 101 101 111 000 001 001
+    --Index4 : 220738P - 101 101 111 001 000 010
+
     -- Test case 1: Set the counter
     Reset <= '0';
-    Data  <= "101";
+    Data  <= "110";
     wait for period;
-    assert Output = "101" report "Test case 1 failed: set not working" severity error;
-    -- Test case 2: Reset the counter
+    assert Output = "110" report "Test case 1 failed: set not working" severity error;
+
+    -- Test case 2:
+    Data  <= "100";
+    wait for period;
+    assert Output = "100" report "Test case 2 failed: set not working" severity error;
+
+    -- Test case 3:
+    Data  <= "011";
+    wait for period;
+    assert Output = "011" report "Test case 3 failed: set not working" severity error;
+
+    -- Test case 4: Reset the counter
     Reset <= '1';
     wait for period;
-    assert Output = "000" report "Test case 2 failed: Reset not working" severity error;
+    assert Output = "000" report "Test case 4 failed: Reset not working" severity error;
 
     -- Test case 3: Increment the counter
     for i in 0 to 7 loop
+
       Reset <= '0';
       Data  <= std_logic_vector(to_unsigned(i, 3));
       wait for period;
       assert Output = std_logic_vector(to_unsigned(i, 3)) report "Test case 3 failed: Increment not working" severity error;
+
     end loop;
 
     wait; -- Wait forever to keep the simulation running
