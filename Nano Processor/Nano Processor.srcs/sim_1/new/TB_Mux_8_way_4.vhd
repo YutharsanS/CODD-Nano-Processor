d684b18eccd358af2f4f58b09300e8bb487d01c3
Vihanga Munasinghe <149761186+VihangaMunasinghe@users.noreply.github.com>
2024-05-04 15:49:29 +0530

    Indexes included into the Mux_8_way_4 sim

--- Nano Processor/Nano Processor.srcs/sim_1/new/TB_Mux_8_way_4.vhd
+++ Nano Processor/Nano Processor.srcs/sim_1/new/TB_Mux_8_way_4.vhd
@@ -6,107 +6,113 @@ end Mux_8_way_4_tb;
 
 architecture tb of Mux_8_way_4_tb is
 
-    component Mux_8_way_4
-        port (
-            D0 : in std_logic_vector(3 downto 0);
-            D1 : in std_logic_vector(3 downto 0);
-            D2 : in std_logic_vector(3 downto 0);
-            D3 : in std_logic_vector(3 downto 0);
-            D4 : in std_logic_vector(3 downto 0);
-            D5 : in std_logic_vector(3 downto 0);
-            D6 : in std_logic_vector(3 downto 0);
-            D7 : in std_logic_vector(3 downto 0);
-            Sel : in std_logic_vector(2 downto 0);
-            EN : in std_logic;
-            Y : out std_logic_vector(3 downto 0)
-        );
-    end component;
+  component Mux_8_way_4
+    port
+    (
+      D0  : in std_logic_vector(3 downto 0);
+      D1  : in std_logic_vector(3 downto 0);
+      D2  : in std_logic_vector(3 downto 0);
+      D3  : in std_logic_vector(3 downto 0);
+      D4  : in std_logic_vector(3 downto 0);
+      D5  : in std_logic_vector(3 downto 0);
+      D6  : in std_logic_vector(3 downto 0);
+      D7  : in std_logic_vector(3 downto 0);
+      Sel : in std_logic_vector(2 downto 0);
+      EN  : in std_logic;
+      Y   : out std_logic_vector(3 downto 0)
+    );
+  end component;
 
-    signal D0_tb, D1_tb, D2_tb, D3_tb, D4_tb, D5_tb, D6_tb, D7_tb : std_logic_vector(3 downto 0);
-    signal Sel_tb : std_logic_vector(2 downto 0);
-    signal EN_tb : std_logic;
-    signal Y_tb : std_logic_vector(3 downto 0);
+  signal D0_tb, D1_tb, D2_tb, D3_tb, D4_tb, D5_tb, D6_tb, D7_tb : std_logic_vector(3 downto 0);
+  signal Sel_tb                                                 : std_logic_vector(2 downto 0);
+  signal EN_tb                                                  : std_logic;
+  signal Y_tb                                                   : std_logic_vector(3 downto 0);
 
 begin
 
-    DUT : Mux_8_way_4
-    port map(
-        D0 => D0_tb,
-        D1 => D1_tb,
-        D2 => D2_tb,
-        D3 => D3_tb,
-        D4 => D4_tb,
-        D5 => D5_tb,
-        D6 => D6_tb,
-        D7 => D7_tb,
-        Sel => Sel_tb,
-        EN => EN_tb,
-        Y => Y_tb
-    );
+  DUT : Mux_8_way_4
+  port map
+  (
+    D0  => D0_tb,
+    D1  => D1_tb,
+    D2  => D2_tb,
+    D3  => D3_tb,
+    D4  => D4_tb,
+    D5  => D5_tb,
+    D6  => D6_tb,
+    D7  => D7_tb,
+    Sel => Sel_tb,
+    EN  => EN_tb,
+    Y   => Y_tb
+  );
 
-    process
-    begin
+  process
+  begin
 
-        D0_tb <= "0000";
-        D1_tb <= "0001";
-        D2_tb <= "0010";
-        D3_tb <= "0011";
-        D4_tb <= "0100";
-        D5_tb <= "0101";
-        D6_tb <= "0110";
-        D7_tb <= "0111";
+    --Index1 : 220038U - 10 1101 1011 1000 0110
+    --Index2 : 220404N - 10 1101 1100 1111 0100
+    --Index3 : 220681H - 10 1101 1110 0000 1001
+    --Index4 : 220738P - 10 1101 1110 0100 0010
 
+    D0_tb <= "1101";
+    D1_tb <= "1000";
+    D2_tb <= "1110";
+    D3_tb <= "1100";
+    D4_tb <= "0000";
+    D5_tb <= "0100";
+    D6_tb <= "0010";
+    D7_tb <= "1111";
+    
+    EN_tb <= '0';
 
-        EN_tb <= '0';
+    -- Test case 1
+    Sel_tb <= "010";
+    wait for 50 ns;
+    assert Y_tb = "1101" report "Test case 1 failed : enable off" severity error;
 
-        -- Test case 1
-        Sel_tb <= "010";
-        wait for 50 ns;
-        assert Y_tb = "0000" report "Test case 1 failed : enable off" severity error;
+    EN_tb <= '1';
 
-        EN_tb <= '1';
+    -- Test case 2
+    Sel_tb <= "000";
+    wait for 50 ns;
+    assert Y_tb = D0_tb report "Test case 2 failed" severity error;
 
-        -- Test case 2
-        Sel_tb <= "000";
-        wait for 50 ns;
-        assert Y_tb = D0_tb report "Test case 2 failed" severity error;
+    -- Test case 3
+    Sel_tb <= "001";
+    wait for 50 ns;
+    assert Y_tb = D1_tb report "Test case 3 failed" severity error;
 
-        -- Test case 3
-        Sel_tb <= "001";
-        wait for 50 ns;
-        assert Y_tb = D1_tb report "Test case 3 failed" severity error;
+    -- Test case 4
+    Sel_tb <= "010";
+    wait for 50 ns;
+    assert Y_tb = D2_tb report "Test case 4 failed" severity error;
 
-        -- Test case 4
-        Sel_tb <= "010";
-        wait for 50 ns;
-        assert Y_tb = D2_tb report "Test case 4 failed" severity error;
+    -- Test case 5
+    Sel_tb <= "011";
+    wait for 50 ns;
+    assert Y_tb = D3_tb report "Test case 5 failed" severity error;
 
-        -- Test case 5
-        Sel_tb <= "011";
-        wait for 50 ns;
-        assert Y_tb = D3_tb report "Test case 5 failed" severity error;
+    -- Test case 6
+    Sel_tb <= "100";
+    wait for 50 ns;
+    assert Y_tb = D4_tb report "Test case 6 failed" severity error;
 
-        -- Test case 6
-        Sel_tb <= "100";
-        wait for 50 ns;
-        assert Y_tb = D4_tb report "Test case 6 failed" severity error;
+    -- Test case 7
+    Sel_tb <= "101";
+    wait for 50 ns;
+    assert Y_tb = D5_tb report "Test case 7 failed" severity error;
 
-        -- Test case 7
-        Sel_tb <= "101";
-        wait for 50 ns;
-        assert Y_tb = D5_tb report "Test case 7 failed" severity error;
+    -- Test case 8
+    Sel_tb <= "110";
+    wait for 50 ns;
+    assert Y_tb = D6_tb report "Test case 8 failed" severity error;
 
-        -- Test case 8
-        Sel_tb <= "110";
-        wait for 50 ns;
-        assert Y_tb = D6_tb report "Test case 8 failed" severity error;
+    -- Test case 9
+    Sel_tb <= "111";
+    wait for 50 ns;
+    assert Y_tb = D7_tb report "Test case 9 failed" severity error;
 
-        -- Test case 9
-        Sel_tb <= "111";
-        wait for 50 ns;
-        assert Y_tb = D7_tb report "Test case 9 failed" severity error;
-
-        wait;
-    end process;
+    wait;
+  end process;
 
 end tb;
